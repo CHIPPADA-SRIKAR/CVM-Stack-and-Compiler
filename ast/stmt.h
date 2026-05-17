d13907a862d4d9f83d7e70657fd51311bcdde633
CHIPPADA-SRIKAR <chippadasrikar787@gmail.com>
2026-05-17 18:53:08 +0530

    Initial commit: Complete CVM++ Compiler with Block Scoping and Stack-Based VM

---
+++ ast/stmt.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <vector>
+
+#include "expr.h"
+
+class Stmt
+{
+public:
+    virtual ~Stmt() {}
+};
+class ExpressionStmt : public Stmt
+{
+public:
+    Expr *expression;
+
+    ExpressionStmt(Expr *expr)
+    {
+        expression = expr;
+    }
+};
+
+class PrintStmt : public Stmt
+{
+public:
+    Expr *expression;
+
+    PrintStmt(Expr *expr)
+    {
+        expression = expr;
+    }
+};
+
+class Program
+{
+public:
+    std::vector<Stmt *> statements;
+};
+
+class IfStmt : public Stmt
+{
+public:
+    Expr *condition;
+
+    std::vector<Stmt *> thenBranch;
+
+    std::vector<Stmt *> elseBranch;
+
+    IfStmt(Expr *cond)
+    {
+        condition = cond;
+    }
+};
+
+class WhileStmt : public Stmt
+{
+public:
+    Expr *condition;
+
+    std::vector<Stmt *> body;
+
+    WhileStmt(Expr *cond)
+    {
+        condition = cond;
+    }
+};
